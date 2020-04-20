9ba19b6f902a2be45d9bf878152e7615dd84510d
hirokiaihara <hiro0818012359@gmail.com>
2020-04-20 15:55:42 +0900

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root 'recipes#index'
   resources :users, only: [:edit, :update]
+  get "users/:id/myrecipes" => "users#myrecipes"
   resources :recipes do
     resources :reports, only: [:create]
+    collection do
+      get 'search'
+    end
   end
+  post "myrecipes/:recipe_id/create" => "myrecipes#create"
+  post "myrecipes/:recipe_id/destroy" => "myrecipes#destroy"
 end
