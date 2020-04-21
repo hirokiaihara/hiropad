12fe10f76481846a0df4669d39b8c1abf4a765ae
hirokiaihara <hiro0818012359@gmail.com>
2020-04-21 22:55:07 +0900

    商品詳細ページのマークアップ

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,5 +1,6 @@
 class Report < ApplicationRecord
   validates :comment, :report_image, presence: true
+  validates :comment, length: {maximum: 32}
   belongs_to :recipe
   belongs_to :user 
   mount_uploader :report_image, ImageUploader
