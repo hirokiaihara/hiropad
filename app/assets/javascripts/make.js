f0e6579b08390a248c30681605bd0cdef6b351c0
hirokiaihara <hiro0818012359@gmail.com>
2020-04-23 21:08:02 +0900

    プレビューの削除に成功！！

--- app/assets/javascripts/make.js
+++ app/assets/javascripts/make.js
@@ -6,6 +6,7 @@ $(document).on('turbolinks:load', ()=> {
     const html = `<img data-index="${index}" src="${url}" width="140px">`;
     return html;
   }
+
   $('#image-box1').on('change', '.js-file', function(e) {
     const targetIndex = $(this).parent().data('index');
     // ファイルのブラウザ上でのURLを取得する
@@ -14,15 +15,26 @@ $(document).on('turbolinks:load', ()=> {
     // 該当indexを持つimgがあれば取得して変数imgに入れる(画像変更の処理)
     if (img = $(`img[data-index="${targetIndex}"]`)[0]) {
       img.setAttribute('src', blobUrl);
+      $('#remove-btn').show();
     } else {  // 新規画像追加の処理
       $('#previews1').append(buildImg(targetIndex, blobUrl));
     }
   });
-
   $("#image-box1").change(function(){
-    $('.present_img1').remove();
+    $('.present_img1').hide();
+    $('#remove-btn').show();
     readURL(this);
   })
+  //削除ボタン
+  $('#remove-btn').on('click', function(){
+    $('#previews1').remove();
+    $(this).hide();
+    $('.present_img1').show();
+    const html = `<div id="previews1">
+                      <input class='js-file hidden', type='file', name="recipe[makes_attributes][0][image]", id="recipe_makes_attributes_0_image" class='js-file hidden' >
+                    </div>`
+    $('#image-box1').append(html);
+  });
 });
 //2
 $(document).on('turbolinks:load', ()=> {
@@ -39,15 +51,26 @@ $(document).on('turbolinks:load', ()=> {
     // 該当indexを持つimgがあれば取得して変数imgに入れる(画像変更の処理)
     if (img = $(`img[data-index="${targetIndex}"]`)[0]) {
       img.setAttribute('src', blobUrl);
+      $('#remove-btn2').show();
     } else {  // 新規画像追加の処理
       $('#previews2').append(buildImg(targetIndex, blobUrl));
     }
   });
-
   $("#image-box2").change(function(){
-    $('.present_img2').remove();
+    $('.present_img2').hide();
+    $('#remove-btn2').show();
     readURL(this);
   })
+  //削除ボタン
+  $('#remove-btn2').on('click', function(){
+    $('#previews2').remove();
+    $(this).hide();
+    $('.present_img2').show();
+    const html = `<div id="previews2">
+                      <input class='js-file hidden', type='file', name="recipe[makes_attributes][1][image]", id="recipe_makes_attributes_1_image" class='js-file hidden' >
+                    </div>`
+    $('#image-box2').append(html);
+  });
 });
 //3
 $(document).on('turbolinks:load', ()=> {
@@ -64,15 +87,26 @@ $(document).on('turbolinks:load', ()=> {
     // 該当indexを持つimgがあれば取得して変数imgに入れる(画像変更の処理)
     if (img = $(`img[data-index="${targetIndex}"]`)[0]) {
       img.setAttribute('src', blobUrl);
+      $('#remove-btn3').show();
     } else {  // 新規画像追加の処理
       $('#previews3').append(buildImg(targetIndex, blobUrl));
     }
   });
-
   $("#image-box3").change(function(){
-    $('.present_img3').remove();
+    $('.present_img3').hide();
+    $('#remove-btn3').show();
     readURL(this);
   })
+  //削除ボタン
+  $('#remove-btn3').on('click', function(){
+    $('#previews3').remove();
+    $(this).hide();
+    $('.present_img3').show();
+    const html = `<div id="previews3">
+                      <input class='js-file hidden', type='file', name="recipe[makes_attributes][2][image]", id="recipe_makes_attributes_2_image" class='js-file hidden' >
+                    </div>`
+    $('#image-box3').append(html);
+  });
 });
 //4
 $(document).on('turbolinks:load', ()=> {
@@ -89,15 +123,27 @@ $(document).on('turbolinks:load', ()=> {
     // 該当indexを持つimgがあれば取得して変数imgに入れる(画像変更の処理)
     if (img = $(`img[data-index="${targetIndex}"]`)[0]) {
       img.setAttribute('src', blobUrl);
+      $('#remove-btn4').show();
     } else {  // 新規画像追加の処理
       $('#previews4').append(buildImg(targetIndex, blobUrl));
     }
   });
 
   $("#image-box4").change(function(){
-    $('.present_img4').remove();
+    $('.present_img4').hide();
+    $('#remove-btn4').show();
     readURL(this);
   })
+  //削除ボタン
+  $('#remove-btn4').on('click', function(){
+    $('#previews4').remove();
+    $(this).hide();
+    $('.present_img4').show();
+    const html = `<div id="previews4">
+                      <input class='js-file hidden', type='file', name="recipe[makes_attributes][3][image]", id="recipe_makes_attributes_3_image" class='js-file hidden' >
+                    </div>`
+    $('#image-box4').append(html);
+  });
 });
 //5
 $(document).on('turbolinks:load', ()=> {
@@ -114,15 +160,27 @@ $(document).on('turbolinks:load', ()=> {
     // 該当indexを持つimgがあれば取得して変数imgに入れる(画像変更の処理)
     if (img = $(`img[data-index="${targetIndex}"]`)[0]) {
       img.setAttribute('src', blobUrl);
+      $('#remove-btn5').show();
     } else {  // 新規画像追加の処理
       $('#previews5').append(buildImg(targetIndex, blobUrl));
     }
   });
 
   $("#image-box5").change(function(){
-    $('.present_img5').remove();
+    $('.present_img5').hide();
+    $('#remove-btn5').show();
     readURL(this);
   })
+  //削除ボタン
+  $('#remove-btn5').on('click', function(){
+    $('#previews5').remove();
+    $(this).hide();
+    $('.present_img5').show();
+    const html = `<div id="previews5">
+                      <input class='js-file hidden', type='file', name="recipe[makes_attributes][4][image]", id="recipe_makes_attributes_4_image" class='js-file hidden' >
+                    </div>`
+    $('#image-box1').append(html);
+  });
 });
 //6
 $(document).on('turbolinks:load', ()=> {
@@ -139,13 +197,25 @@ $(document).on('turbolinks:load', ()=> {
     // 該当indexを持つimgがあれば取得して変数imgに入れる(画像変更の処理)
     if (img = $(`img[data-index="${targetIndex}"]`)[0]) {
       img.setAttribute('src', blobUrl);
+      $('#remove-btn6').show();
     } else {  // 新規画像追加の処理
       $('#previews6').append(buildImg(targetIndex, blobUrl));
     }
   });
 
   $("#image-box6").change(function(){
-    $('.present_img6').remove();
+    $('.present_img6').hide();
+    $('#remove-btn6').show();
     readURL(this);
   })
+  //削除ボタン
+  $('#remove-btn6').on('click', function(){
+    $('#previews6').remove();
+    $(this).hide();
+    $('.present_img6').show();
+    const html = `<div id="previews6">
+                      <input class='js-file hidden', type='file', name="recipe[makes_attributes][5][image]", id="recipe_makes_attributes_5_image" class='js-file hidden' >
+                    </div>`
+    $('#image-box6').append(html);
+  });
 });
