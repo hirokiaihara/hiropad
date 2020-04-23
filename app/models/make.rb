a3c9832b51ed9dbf5d7f5608f016ff35fa054102
hirokiaihara <hiro0818012359@gmail.com>
2020-04-23 10:48:32 +0900

    軽微な修正

--- app/models/make.rb
+++ app/models/make.rb
@@ -1,6 +1,5 @@
 class Make < ApplicationRecord
   belongs_to :recipe, optional: true
-  # validates :image, :how, presence: true
-  # validates :how_make, length: {maximum: 60}
+  validates :how_make, length: {maximum: 60}
   mount_uploader :image, ImageUploader
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :name, :year, :month, :day, presence: true
+  validates :name, length: {maximum: 10}
+  validates :users_comment, length: {maximum: 120}
   has_many :recipes
   has_many :reports
   mount_uploader :user_image, ImageUploader
