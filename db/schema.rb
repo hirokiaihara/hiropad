def3419060b9babdc377867ef7255e99401f2374
hirokiaihara <hiro0818012359@gmail.com>
2020-04-24 16:11:26 +0900

    カラムの制約の修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200422064403) do
+ActiveRecord::Schema.define(version: 20200424055124) do
 
   create_table "foods", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "food_name",  null: false
@@ -22,8 +22,8 @@ ActiveRecord::Schema.define(version: 20200422064403) do
   end
 
   create_table "makes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.text     "how_make",   limit: 65535, null: false
     t.string   "image"
+    t.text     "how_make",   limit: 65535
     t.integer  "recipe_id"
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
